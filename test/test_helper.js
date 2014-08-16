c7c47cdd4d23452de7d9e4ab372aea43adbd707b
Shogo Iwano <shiwano@gmail.com>
2014-08-16 09:14:03 +0900

    Fix test

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,16 +3,7 @@
 var fs = require('fs');
 var nock = require('nock');
 
-function str2ab(str) {
-  var buf = new Buffer(str.length * 2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf);
-  for (var i = 0; i < str.length; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-  return buf;
-}
-
 global.nockScope = nock('http://testflightapp.com')
   .post('/api/builds.json').reply(200, function() {
-      return str2ab(fs.readFileSync('./test/fixtures/result.json', 'utf8'));
+      return fs.readFileSync('./test/fixtures/result.json', 'utf8');
   });
